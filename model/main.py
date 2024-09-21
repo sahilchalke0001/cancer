fe5aae97c5c8661134c6eb0e0d5902e655b98cee
Sahil11122 <sahilchalke21@gmail.com>
2024-09-21 22:43:19 +0530

    first commit

---
+++ model/main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle
+
+
+def create_model(data):
+  X = data.drop(['diagnosis'], axis=1)##predict
+  y = data['diagnosis']##traget
+
+  # scale the data
+  scaler = StandardScaler()
+  X = scaler.fit_transform(X)
+
+  #split the data
+  X_train,X_test,y_train,y_test = train_test_split(
+    X,y, test_size=0.2, random_state=42
+  )
+
+  #train the model
+  model = LogisticRegression()
+  model.fit(X_train,y_train)
+
+  #test model
+  y_pred = model.predict(X_test)
+  print('Accuracy of our model: ', accuracy_score(y_test,y_pred))
+  print("Classification report: \n", classification_report(y_test, y_pred))
+
+  return model, scaler
+
+
+
+def get_clean_data():
+  data=pd.read_csv(r"C:\Users\SAHIL\Desktop\Breast_cancer\data\data.csv")
+  data = data.drop(['id'],axis=1)  
+  data = data.drop(['Unnamed: 32'],axis = 1)
+  ##Change maglignant = 1 and benine = 0
+  data['diagnosis']=data['diagnosis'].map({'M':1,'B':0})
+
+  return data
+
+def main():
+  data = get_clean_data()
+ 
+  #Create the model
+  model,scaler = create_model(data)
+
+  ##we dump the model in picke file because to stop the model from rerendering everytime and save time
+  with open('model/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+  with open('model/scaler.pkl', 'wb') as f:
+    pickle.dump(scaler, f)
+
+
+if __name__ == '__main__':
+  main()
+
+
